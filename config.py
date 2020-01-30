ecbd4089787eb3c8e925dc4bfee417b378e4774b
Abhishek <a@a6k.me>
2020-01-30 11:27:07 +0530

    kafka connect working

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ NUM_REPLICAS = 1
 REST_PROXY_URL = "http://localhost:8082"
 KAFKA_URL = "PLAINTEXT://localhost:9092"
 SCHEMA_REGISTRY_URL = "http://localhost:8081"
+DATABASE_URL = "jdbc:postgresql://postgres:5432/cta"
+DATABASE_USER = "cta_admin"
+DATABASE_PASSWORD = "chicago"
